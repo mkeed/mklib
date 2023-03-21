724417a8c7fc6e1cb558078dd36c3616ee625453
Mitch Kennedy <mitchell.kennedy@citadelhealth.com.au>
2023-03-22 10:32:33 +1100

    glyf stuff

--- font/src/tables/glyf.zig
+++ font/src/tables/glyf.zig
@@ -6,7 +6,7 @@ const StraightLine = struct {
     end: Point,
 };
 
-const CurveLine = struct {
+const CurvedLine = struct {
     start: Point,
     control1: Point,
     Control2: Point,
@@ -46,7 +46,15 @@ pub fn parse(data: []const u8, alloc: std.mem.Allocator, numGlyfs: usize) !GLYF 
     var fbs = std.io.fixedBufferStream(data);
     const reader = fbs.reader();
     std.log.info("data:[{}]", .{std.fmt.fmtSliceHexUpper(data[0..100])});
+    var xPointBuffer = std.ArrayList(i16).init(alloc);
+    defer xPointBuffer.deinit();
+    var yPointBuffer = std.ArrayList(i16).init(alloc);
+    defer yPointBuffer.deinit();
+    var isCurveBuffer = std.ArrayList(bool).init(alloc);
+    defer isCurveBuffer.deinit();
     for (0..numGlyfs) |glyfIdx| {
+        xPointBuffer.clearRetainingCapacity();
+        yPointBuffer.clearRetainingCapacity();
         const numberOfContours = try reader.readIntBig(i16);
         if (numberOfContours < 0) return error.CompositeNotImplemented;
         const num = @intCast(u16, numberOfContours);
@@ -59,7 +67,7 @@ pub fn parse(data: []const u8, alloc: std.mem.Allocator, numGlyfs: usize) !GLYF 
         for (0..num) |_| {
             const endPoint = try reader.readIntBig(u16);
             last = endPoint;
-            std.log.info("point:{}", .{endPoint});
+            //std.log.info("point:{}", .{endPoint});
         }
         const instructionLength = try reader.readIntBig(u16);
         std.log.info("instructionLength:{}", .{instructionLength});
@@ -75,25 +83,31 @@ pub fn parse(data: []const u8, alloc: std.mem.Allocator, numGlyfs: usize) !GLYF 
             const flag = @ptrCast(*const Flag, &flagVal);
             try flags.append(flag.*);
         }
-        var curPoint: isize = 0;
-        std.log.info("vals:{}", .{std.fmt.fmtSliceHexUpper(data[fbs.pos .. fbs.pos + 20])});
+        var curPoint: i16 = 0;
+        //std.log.info("vals:{}", .{std.fmt.fmtSliceHexUpper(data[fbs.pos .. fbs.pos + 20])});
         for (flags.items) |f| {
-            std.log.info("flags:{}", .{f});
+            //std.log.info("flags:{}", .{f});
             const val = if (f.xShort == 1)
                 @intCast(i16, try reader.readIntBig(u8)) * if (f.xSame == 1) @as(i16, 1) else @as(i16, -1)
             else if (f.xSame == 1) @as(i16, 0) else try reader.readIntBig(i16);
+
             curPoint += val;
+            try xPointBuffer.append(curPoint);
             std.log.info("val:{} x:{}", .{ val, curPoint });
         }
         curPoint = 0;
-        std.log.info("vals:{}", .{std.fmt.fmtSliceHexUpper(data[fbs.pos .. fbs.pos + 20])});
+        //std.log.info("vals:{}", .{std.fmt.fmtSliceHexUpper(data[fbs.pos .. fbs.pos + 20])});
         for (flags.items) |f| {
-            std.log.info("flags:{}", .{f});
+            //std.log.info("flags:{}", .{f});
             const val = if (f.yShort == 1)
                 @intCast(i16, try reader.readIntBig(u8)) * if (f.ySame == 1) @as(i16, 1) else @as(i16, -1)
             else if (f.ySame == 1) @as(i16, 0) else try reader.readIntBig(i16);
             curPoint += val;
-            std.log.info("val:{} y:{}", .{ val, curPoint });
+            try yPointBuffer.append(curPoint);
+            //std.log.info("val:{} y:{}", .{ val, curPoint });
+        }
+        for (xPointBuffer.items, yPointBuffer.items) |x, y| {
+            std.log.info("point:[{}:{}]", .{ x, y });
         }
         if (glyfIdx > 2) break;
     }
