03f0e3edd9268f2a164a67ede4e6e89545bbf27a
Mitch Kennedy <mitchk@runbox.com>
2023-04-21 21:28:43 +1000

    work

---
+++ Edit/src/Log.zig
@@ -0,0 +1,46 @@
+const std = @import("std");
+
+const LogItem = struct {
+    level: std.log.Level,
+    info: std.ArrayList(u8),
+    pub fn init(alloc: std.mem.Allocator, level: std.log.Level) LogItem {
+        return .{
+            .level = level,
+            .info = std.ArrayList(u8).init(alloc),
+        };
+    }
+    pub fn deinit(self: LogItem) void {
+        self.info.deinit();
+    }
+};
+
+pub const Logger = struct {
+    pub const LoggerOpts = struct {
+        maxDepth: usize = 100,
+    };
+    alloc: std.mem.Allocator,
+    logs: std.ArrayList(LogItem),
+    opts: LoggerOpts,
+    pub fn init(alloc: std.mem.Allocator, opts: LoggerOpts) Logger {
+        return .{
+            .alloc = alloc,
+            .logs = std.ArrayList(LogItem).init(alloc),
+            .opts = opts,
+        };
+    }
+    pub fn deinit(self: Logger) void {
+        for (self.logs.items) |item| {
+            item.deinit();
+        }
+        self.logs.deinit();
+    }
+    pub fn err(self: *Logger, comptime fmt: []const u8, args: anytype) void {
+        self.errImpl(fmt, args);
+    }
+    fn errImpl(self: *Logger, comptime fmt: []const u8, args: anytype) !void {
+        var line = LogItem.init(self.alloc, .err);
+        errdefer line.deinit();
+        try std.fmt.format(line.info.writer(), fmt, args);
+        try selg.logs.append(line);
+    }
+};
--- Edit/src/MkedCore.zig
+++ Edit/src/MkedCore.zig
@@ -1,4 +1,5 @@
 const std = @import("std");
+const Log = @import("Log.zig");
 
 pub const FileHandle = struct {};
 
@@ -11,10 +12,12 @@ const FileInfo = struct {
 pub const MkedCore = struct {
     projectDir: std.fs.Dir,
     files: std.ArrayList(?FileInfo),
+    errorLog: Log.Logger,
     pub fn init(alloc: std.mem.Allocator) MkedCore {
         return MkedCore{
             .projectDir = std.fs.cwd(),
             .files = std.ArrayList(?FileInfo).init(alloc),
+            .errorLog = Log.Logger.init(alloc),
         };
     }
     pub fn deinit(self: *MkedCore) void {
@@ -24,5 +27,13 @@ pub const MkedCore = struct {
             }
         }
         self.files.deinit();
+        self.errorLog.deinit();
+    }
+
+    pub fn openFileRead(self: *MkedCore, name: []const u8) !FileHandle {
+        var file = self.projectDir.openFile(name, .{}) catch |err| {
+            self.errorLog.err("Failed To Open File with error({})", .{err});
+            return error.FailedToOpen;
+        };
     }
 };
