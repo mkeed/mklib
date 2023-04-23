802f55fc78573a20afdfe31c6d997073765b4723
Mitch Kennedy <mitchk@runbox.com>
2023-04-23 19:39:13 +1000

    eventloop startin to work

--- Edit/src/EventLoop.zig
+++ Edit/src/EventLoop.zig
@@ -7,7 +7,7 @@ pub const HandlerError = error{
 
 pub const HandlerResult = enum {
     None,
-    Exit,
+    Done,
 };
 
 pub const FnHandler = *const fn (ctx: *anyopaque, fd: std.os.fd_t) HandlerError!HandlerResult;
@@ -32,34 +32,146 @@ pub const HandlerInfo = struct {
             .revents = 0,
         };
     }
-};
+    pub const HandleResult = enum {
+        Close,
+        EndProgram,
+        None,
 
-pub const SignalHandler = struct {
-    ctx: *anyopaque,
-    func: FnHandler,
+        pub fn combine(self: HandleResult, other: HandleResult) HandleResult {
+            switch (self) {
+                .EndProgram => return .EndProgram,
+                .Close => {
+                    if (other == .EndProgram) {
+                        return .EndProgram;
+                    } else {
+                        return .Close;
+                    }
+                },
+                .None => {
+                    return other;
+                },
+            }
+        }
+    };
+    fn callFunc(self: HandlerInfo, func: FnHandler) HandleResult {
+        const result = func(self.ctx, self.fd) catch |err| {
+            switch (err) {
+                error.GlobalFatal => return .EndProgram,
+                error.LocalFatal => return .Close,
+            }
+        };
+        switch (result) {
+            .None => return .None,
+            .Done => return .Close,
+        }
+    }
+    pub fn handleEvents(self: HandlerInfo, revents: i16) HandleResult {
+        var result: HandleResult = .None;
+        if ((revents & std.os.POLL.IN) != 0) {
+            if (self.read) |func| {
+                result = result.combine(self.callFunc(func));
+            }
+        }
+        if ((revents & std.os.POLL.OUT) != 0) {
+            if (self.write) |func| {
+                result = result.combine(self.callFunc(func));
+            }
+        }
+        if ((revents & std.os.POLL.PRI) != 0) {
+            if (self.pri) |func| {
+                result = result.combine(self.callFunc(func));
+            }
+        }
+        if ((revents & std.os.POLL.HUP) != 0) {
+            if (self.hup) |func| {
+                result = result.combine(self.callFunc(func));
+            } else {
+                result = .Close;
+            }
+        }
+        if ((revents & std.os.POLL.ERR) != 0) {
+            if (self.err) |func| {
+                result = result.combine(self.callFunc(func));
+            } else {
+                result = .Close;
+            }
+        }
+        if ((revents & std.os.POLL.NVAL) != 0) {
+            result = .Close;
+        }
+        return result;
+    }
+    pub fn deinit(self: HandlerInfo) void {
+        if (self.exit) |func| {
+            _ = self.callFunc(func);
+        }
+    }
 };
 
 pub const EventLoop = struct {
     alloc: std.mem.Allocator,
     pollfds: std.ArrayList(std.os.pollfd),
     handlers: std.ArrayList(HandlerInfo),
-    signalHandlers: [std.os.NSIG]?SignalHandler,
     pub fn init(alloc: std.mem.Allocator) EventLoop {
         return EventLoop{
             .alloc = alloc,
             .pollfds = std.ArrayList(std.os.pollfd).init(alloc),
-            .handlers = std.ArrayList(HandlerInfo),
+            .handlers = std.ArrayList(HandlerInfo).init(alloc),
         };
+    }
+    pub fn deinit(self: EventLoop) void {
+        for (self.handlers.items) |handler| {
+            if (handler.exit) |func| {
+                _ = func(handler.ctx, handler.fd) catch {};
+            } else {
+                std.os.close(handler.fd);
+            }
+        }
+        self.handlers.deinit();
+        self.pollfds.deinit();
+    }
+
+    pub fn addHandler(self: *EventLoop, handler: HandlerInfo) !void {
+        try self.handlers.append(handler);
     }
 
     pub fn run(self: *EventLoop) !void {
-        while (true) {
+        var toRemove = std.ArrayList(std.os.fd_t).init(self.alloc);
+        defer toRemove.deinit();
+        mainLoop: while (self.handlers.items.len > 0) {
             try self.pollfds.ensureTotalCapacity(self.handlers.items.len);
             self.pollfds.clearRetainingCapacity();
-            for (self.handlers.items, 0..) |handler, idx| {
+            for (self.handlers.items) |handler| {
                 try self.pollfds.append(handler.pollfd());
             }
-            //const num = std.os.poll(self.pollfds.items,
+            const num = std.os.poll(self.pollfds.items, -1) catch |err| {
+                std.log.err("System Failed: {}", .{err});
+                break :mainLoop;
+            };
+            if (num == 0) {
+                continue :mainLoop;
+            }
+            for (self.pollfds.items, 0..) |pollfd, idx| {
+                if (pollfd.revents != 0) {
+                    switch (self.handlers.items[idx].handleEvents(pollfd.revents)) {
+                        .Close => {
+                            try toRemove.append(pollfd.fd);
+                        },
+                        .EndProgram => {
+                            break :mainLoop;
+                        },
+                        .None => {},
+                    }
+                }
+            }
+            for (toRemove.items) |fd| {
+                for (self.handlers.items, 0..) |item, idx| {
+                    if (item.fd == fd) {
+                        item.deinit();
+                        _ = self.handlers.swapRemove(idx);
+                    }
+                }
+            }
         }
     }
 };
--- Edit/src/main.zig
+++ Edit/src/main.zig
@@ -1,11 +1,32 @@
 const std = @import("std");
-const mked = @import("mked.zig");
+const EventLoop = @import("EventLoop.zig");
+
+fn readExample(ctx: *anyopaque, fd: std.os.fd_t) EventLoop.HandlerError!EventLoop.HandlerResult {
+    var data: [512]u8 = undefined;
+    const len = std.os.read(fd, data[0..]) catch {
+        return EventLoop.HandlerResult.Done;
+    };
+
+    std.log.info("data:[{s}]", .{data[0..len]});
+    _ = ctx;
+    return EventLoop.HandlerResult.Done;
+}
 
 pub fn main() !void {
     var gpa = std.heap.GeneralPurposeAllocator(.{}){};
     defer _ = gpa.deinit();
     const alloc = gpa.allocator();
-    try mked.run(alloc);
+
+    var eventLoop = EventLoop.EventLoop.init(alloc);
+    defer eventLoop.deinit();
+    var in = std.io.getStdIn();
+    try eventLoop.addHandler(.{
+        .fd = in.handle,
+        .ctx = &gpa,
+        .read = &readExample,
+    });
+    //
+    try eventLoop.run();
 }
 
 test "simple test" {
