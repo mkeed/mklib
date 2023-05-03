ced40dfaa8bc91a201986e21498dbe97bd6c166e
Mitch Kennedy <mitchell.kennedy@citadelhealth.com.au>
2023-05-03 17:03:58 +1000

    work

--- Edit/src/BufferView.zig
+++ Edit/src/BufferView.zig
@@ -2,16 +2,10 @@ const std = @import("std");
 const Buffer = @import("Buffer.zig");
 const Render = @import("Render.zig");
 
-pub const LinePos = union(enum) {
-    Top: usize,
-    Middle: usize,
-    Bottom: usize,
-};
-
 pub const BufferView = struct {
     buffer: *Buffer.Buffer,
     cursor_set: *Buffer.CursorSet,
-    line: LinePos,
+    row: usize,
     pub fn init(buffer: *Buffer.Buffer) !BufferView {
         const cursor_set = try buffer.createCursorSet();
         return BufferView{
@@ -23,5 +17,27 @@ pub const BufferView = struct {
     pub fn deinit(self: BufferView) void {
         self.buffer.clearCursorSet(self.cursor_set);
     }
-    pub fn render(self: BufferView, size: Render.Pos, arena: std.mem.Allocator) !Render.Window {}
+    pub fn render(self: BufferView, size: Render.Pos, arena: std.mem.Allocator) !Render.Window {
+        const num_lines = std.math.min(self.buffer.numLines() - self.row, size.y);
+        for (0..num_lines) |line| {
+            const row = self.buffer.getLine(line + self.row) orelse unreachable;
+        }
+    }
 };
+
+// 1----------
+// 2
+// 3
+// 4
+// 5
+// 6 m
+// 7
+// 8
+// 9
+// 10
+// 11
+// 12_________
+// 13
+// 14
+// 15
+// 16
