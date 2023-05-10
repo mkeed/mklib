fcb16b6be111dd7ad022e45ec26f20693cb453a1
Mitch Kennedy <mitchk@runbox.com>
2023-05-10 11:56:08 +1000

    work

--- MKGUI/MKGUI.zig
+++ MKGUI/MKGUI.zig
@@ -2,18 +2,36 @@ const std = @import("std");
 
 pub const MKGUIOptions = struct {};
 
-pub fn init(alloc: std.mem.Allocator, opts: MKGUIOptions) !MKGUI {}
+pub fn init(alloc: std.mem.Allocator, opts: MKGUIOptions) !*MKGUI {}
 
 pub const MKGUI = struct {
+    alloc: std.mem.Allocator,
+    screens: std.ArrayList(Screen),
     pub fn init(alloc: std.mem.Allocator) MKGUI {
-        _ = alloc;
-        return .{};
+        return .{
+            .alloc = alloc,
+            .screens = std.ArrayList(Screen).init(alloc),
+        };
     }
     pub fn deinit(self: MKGUI) void {
-        _ = self;
+        for (self.
     }
 
     pub fn run(self: MKGUI) !void {
+        _ = self;
+    }
+    pub fn addScreen(self: MKGUI) !Screen {
+        //
+    }
+};
+
+pub const Screen = struct {
+    alloc: std.mem.Allocator,
+    core: MKGUI,
+    pub fn init(alloc: std.mem.Allocator, core: MKGUI) Screen {
+        return .{ .alloc = alloc, .core = core };
+    }
+    pub fn deinit(self: Screen) void {
         _ = self;
     }
 };
